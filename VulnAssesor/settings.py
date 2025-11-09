e8c650756355a5e59246fce88b0d3f31b453188b
MrWipp <mrwipbussiness@gmail.com>
2025-11-09 11:38:02 +0300

    Core Foundation - No Bugs

--- VulnAssesor/settings.py
+++ VulnAssesor/settings.py
@@ -127,3 +127,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'dashboard'
 LOGOUT_REDIRECT_URL = 'login'
+
+# Static Files
+STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
